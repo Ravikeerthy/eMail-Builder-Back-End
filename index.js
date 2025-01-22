a35daa188a68cb85c3d4d9dd41ce9eac1eb70e88
Ravikeerthy <ravikeerthy1807@gmail.com>
2025-01-22 22:32:24 +0400

    second commit

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ connectDB();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://e-mail-builder.netlify.app/",
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     allowedHeaders: "Content-Type",
   })
