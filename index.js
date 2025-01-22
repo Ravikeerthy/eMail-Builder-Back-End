7b92dd853cf87d2173bbe4faeac1d7ed858c1370
Ravikeerthy <ravikeerthy1807@gmail.com>
2025-01-22 23:09:49 +0400

    cors updated

--- index.js
+++ index.js
@@ -20,17 +20,23 @@ const PORT = process.env.PORT || 5000;
 
 connectDB();
 app.use(express.json());
+app.use(cors());
 app.use(
   cors({
     origin: "https://e-mail-builder.netlify.app/",
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    allowedHeaders: "Content-Type",
+    allowedHeaders: "Content-Type, Authorization",
   })
 );
+
+app.options("*", (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "https://e-mail-builder.netlify.app/");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.sendStatus(200);
+});
 app.use(bodyParser.json());
-
-
-
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/getEmailLayout", (req, res) => {
