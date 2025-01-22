04d7f8a6403ff38a3c1eeb569904a74f05f1b12a
Ravikeerthy <ravikeerthy1807@gmail.com>
2025-01-22 23:31:37 +0400

    cors updated

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "https://e-mail-builder.netlify.app/",
+    origin: "https://e-mail-builder.netlify.app",
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type, Authorization"],
     credentials: true,
